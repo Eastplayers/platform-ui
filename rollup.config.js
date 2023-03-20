475288f76742f91e36d34dc768a6848cc733b4c0
Quyen Le <quyen.le@eastplayers.io>
2023-03-21 01:13:06 +0700

    change package name

--- rollup.config.js
+++ rollup.config.js
@@ -11,7 +11,7 @@ export default [
   {
     input: "src/index.tsx",
     output: {
-      name: "edgee-tracking",
+      name: "platform-ui",
       file: pkg.browser,
       format: "es",
       dir: "lib",
