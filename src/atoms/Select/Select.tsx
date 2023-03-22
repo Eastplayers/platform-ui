b95728f16438ff675036b6fc2a355b5c5fe016a4
Quyen Le <quyen.le@eastplayers.io>
2023-03-22 20:54:59 +0700

    remove default open menu list of select

--- src/atoms/Select/Select.tsx
+++ src/atoms/Select/Select.tsx
@@ -20,7 +20,6 @@ const Select: FC<SelectProps> = ({
     <div className={classNames("pui-field", size)}>
       {label && <label className="pui-input-label">{label}</label>}
       <ReactSelect
-        menuIsOpen
         isClearable={clearable}
         isSearchable={searchable}
         isDisabled={disabled}
