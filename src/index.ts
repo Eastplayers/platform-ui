896fc3fe222fcfdc466b4af867f9ba5c83d29e2e
Quyen Le <quyen.le@eastplayers.io>
2023-03-23 05:03:47 +0700

    add missing export

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import "./styles/main.scss";
 
-export * from "./atoms/Button/Button.types";
+export { default as Avatar } from "./atoms/Avatar";
 export { default as Button } from "./atoms/Button";
-
+export { default as Card } from "./atoms/Card";
 export { default as Checkbox } from "./atoms/Checkbox";
+export { default as Input } from "./atoms/Input";
+export { default as Select } from "./atoms/Select";
 export { default as Text } from "./atoms/Text";
